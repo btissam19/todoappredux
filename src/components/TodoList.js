b822a4303cfdd389cb5d641a6544ce2c596216c8
Ibtissam <btissam1234btissam12@gmail.com>
2023-12-09 22:36:34 +0100

    last changes

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEdit, faTrash, faCircle } from '@fortawesome/free-solid-svg-icons';
+import {  faTrash, faCircle } from '@fortawesome/free-solid-svg-icons';
 import { removeTodo } from '../features/todoSlice';
 import { useDispatch } from 'react-redux';
 
@@ -14,11 +14,10 @@ const TodoList = () => {
       {todos.map((task) => (
         <li key={task.id} className="flex justify-between items-center mb-4 bg-gray-50 p-3 rounded-lg">
           <div className="flex items-center">
-            <FontAwesomeIcon icon={faCircle} className="text-gray-500" />
+            <FontAwesomeIcon icon={faCircle} className="text-cyan-500" />
             <h5 className="ml-2 text-dark">{task.text}</h5>
           </div>
           <div className="task-links flex items-center gap-3">
-            <FontAwesomeIcon icon={faEdit} className="text-green-600" />
             <button type="button" onClick={()=>{dispatch(removeTodo(task.id))}} className="delete-btn text-red-600">
               <FontAwesomeIcon icon={faTrash} />
             </button>
